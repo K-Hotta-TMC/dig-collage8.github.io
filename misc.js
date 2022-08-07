fcde4ba2f48f64a322c2db47033707c530f08a99
K-Hotta-TMC <108398090+K-Hotta-TMC@users.noreply.github.com>
2022-08-08 02:06:26 +0900

    Update misc.js

--- misc.js
+++ misc.js
@@ -1,9 +1,30 @@
-//キーボードを押されたとき
-document.onkeydown = function (e) {
-    key[e.keyCode] = true;
-}
-document.onkeyup = function (e) {
-    key[e.keyCode] = false;
+//星クラス作成
+class StarDraw {
+    constructor() {
+        this.x = rand(0, fieldWidth) << 8; //x座標
+        this.y = rand(0, fieldHeight) << 8; //y座標
+        this.vx = 0; //x方向のベクトル
+        this.vy = rand(30, 200); //y方向のベクトル
+        this.size = rand(1, 2); //星の大きさ
+    }
+    draw() {
+        let x = this.x >> 8
+        let y = this.y >> 8
+        if (x < cameraX || x >= cameraX + screenWidth || y < cameraY || y >= cameraY + screenHeight) {
+            return; //画面外は描画しない
+        } else {
+            vcon.fillStyle = rand(0, 2) != 0 ? "#66f" : "#aef";
+            vcon.fillRect(x, y, this.size, this.size);
+        }
+    }
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+        if (this.y > fieldHeight << 8) {
+            this.y = 0;
+            this.x = rand(0, fieldWidth) << 8;
+        }
+    }
 }
 
 //キャラクターのベースクラス
@@ -22,8 +43,8 @@ class CharaBase {
         this.x += this.vx;
         this.y += this.vy;
 
-        if (this.x < 0 || this.x > fieldWidth << 8 || this.y < 0 || this.y > fieldHeight << 8) {
-            this.kill = true; //自身を消すフラグを立てる
+        if (this.x + (100 << 8) < 0 || this.x - (100 << 8) > fieldWidth << 8 || this.y + (100 << 8) < 0 || this.y - (100 << 8) > fieldHeight << 8) {
+            this.kill = true; //画面外は表示しない
         }
     }
     draw() {
@@ -55,43 +76,13 @@ class Expl extends CharaBase {
     }
 }
 
-//もっと派手な爆発
+//派手な爆発を描画
 function explosion(x, y, vx, vy) {
     expl.push(new Expl(0, x, y, vx, vy));
     for (let i = 0; i < 10; i++) {
         let evx = vx + (rand(-10, 10) << 6);
         let evy = vy + (rand(-10, 10) << 6);
         expl.push(new Expl(i, x, y, evx, evy));
-    }
-}
-
-//星クラス作成
-class StarDraw {
-    constructor() {
-        this.x = rand(0, fieldWidth) << 8; //x座標
-        this.y = rand(0, fieldHeight) << 8; //y座標
-        this.vx = 0; //x方向のベクトル
-        this.vy = rand(30, 200); //y方向のベクトル
-        this.size = rand(1, 2); //星の大きさ
-    }
-    draw() {
-        let x = this.x >> 8
-        let y = this.y >> 8
-        if (x < cameraX || x >= cameraX + screenWidth || y < cameraY || y >= cameraY + screenHeight) {
-            return; //画面外は描画しない
-        } else {
-            vcon.fillStyle = rand(0, 2) != 0 ? "#66f" : "#aef";
-            vcon.fillRect(x, y, this.size, this.size);
-        }
-
-    }
-    update() {
-        this.x += this.vx;
-        this.y += this.vy;
-        if (this.y > fieldHeight << 8) {
-            this.y = 0;
-            this.x = rand(0, fieldWidth) << 8;
-        }
     }
 }
 
@@ -115,27 +106,24 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//当たり判定
-/*
-function checkHit(x1, y1, w1, h1, x2, y2, w2, h2) {
-    //矩形同士の当たり判定
-
-    let left1 = x1 >> 8;
-    let right1 = left1 + w1;
-    let top1 = y1 >> 8
-    let bottom1 = top1 + h1;
-
-    let left2 = x2 >> 8;
-    let right2 = left2 + w2;
-    let top2 = y2 >> 8
-    let bottom2 = top2 + h2;
-    return left1 <= right2 && right1 >= left2 && top1 <= bottom2 && bottom1 >= top2;
-}
- */
-
+//当たり判定を関数化
 function checkHit(x1, y1, r1, x2, y2, r2) {
     let a = (x1 - x2) >> 8
     let b = (y1 - y2) >> 8
     let r = r1 + r2;
     return r * r >= a * a + b * b;
+}
+
+//キーボードを押されたとき
+document.onkeydown = function (e) {
+    key[e.keyCode] = true;
+    if (gameOver && e.keyCode === 82) {
+        delete jiki;
+        jiki = new Jiki();
+        gameOver = false;
+        score = 0;
+    }
+}
+document.onkeyup = function (e) {
+    key[e.keyCode] = false;
 }
